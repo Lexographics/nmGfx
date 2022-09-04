9eb35a9805df218fadd59f28c9082ee6fa78c652
Lexographics <zbamgamesinfo@gmail.com>
2022-09-04 17:08:38 +0300

    Math: Add model matrix calculation functions

---
+++ src/Core/Matrix.cpp
@@ -0,0 +1,49 @@
+#include "Matrix.hpp"
+
+#include "glm/gtc/matrix_transform.hpp"
+
+namespace nmGfx
+{
+    glm::mat4 CalculateModelMatrix(
+        const glm::vec3& translation,
+        const glm::vec3& rotation,
+        const glm::vec3& scale,
+        const glm::vec3& offset /*= glm::vec3(0.f, 0.f, 0.f)*/,
+        const glm::mat4& baseTransform /*= glm::mat4(1.f)*/)
+    {
+        glm::mat4 transform = baseTransform;
+        
+        /*
+        transform = glm::translate(transform, offset);
+        transform = glm::translate(transform, translation);
+        
+        transform = glm::scale(transform, scale);
+
+        transform = glm::rotate(transform, glm::radians(rotation.y), {0.f, 1.f, 0.f});
+        transform = glm::rotate(transform, glm::radians(rotation.x), {1.f, 0.f, 0.f});
+        transform = glm::rotate(transform, glm::radians(rotation.z), {0.f, 0.f, 1.f});
+        */
+
+        transform = glm::translate(transform, offset);
+        
+        transform = glm::scale(glm::mat4(1.f), scale) * transform;
+
+        transform = glm::rotate(glm::mat4(1.f), glm::radians(rotation.z), {0.f, 0.f, 1.f}) * transform;
+        transform = glm::rotate(glm::mat4(1.f), glm::radians(rotation.y), {0.f, 1.f, 0.f}) * transform;
+        transform = glm::rotate(glm::mat4(1.f), glm::radians(rotation.x), {1.f, 0.f, 0.f}) * transform;
+        
+        transform = glm::translate(glm::mat4(1.f), translation) * transform;
+
+        return transform;
+    }
+    
+    glm::mat4 CalculateModelMatrix(
+        const glm::vec2& position,
+        float rotation,
+        const glm::vec2& scale,
+        const glm::vec2& offset /*= glm::vec2(0.f, 0.f)*/,
+        const glm::mat4& baseTransform /*= glm::mat4(1.f)*/)
+    {
+        return CalculateModelMatrix({position.x, position.y, 0.f}, {0.f, 0.f, rotation}, {scale.x, scale.y, 1.f}, {offset.x, offset.y, 0.f}, baseTransform);
+    }
+} // namespace nmGfx
